99f39cc2664ebe0e3f22796cc33ff99284059da2
AndersRKvamme <rka092024@gmail.com>
2025-01-10 10:38:32 +0100

    FUNGERENDE KEYS!

--- js.js
+++ js.js
@@ -25,22 +25,23 @@ console.log(innerHeight)
 console.log(innerWidth)
 // InnerHeight and InnerWidth shows window size for collision. DO NOT HARDCODE.
 
-window.addEventListener("keyup", function(event) {
+window.addEventListener("keydown", function(event) {
     switch (event.key) {
         case 'ArrowUp':
             console.log('Up arrow key pressed');
-        //    todd.style.transform = changePosititon (x , (y-=10));
+            todd.style.top =parseInt(todd.style.top) - moveBy +'px';
             break;
         case 'ArrowDown':
             console.log('Down arrow key pressed');
-        //    todd.style.transform = changePosititon (x+=10 , (y));
-            todd.style.left =parseInt(todd.style.top) - moveBy +'px';
+            todd.style.top =parseInt(todd.style.top) + moveBy +'px';
             break;
         case 'ArrowLeft':
             console.log('Left arrow key pressed');
+            todd.style.left =parseInt(todd.style.left) - moveBy +'px';
             break;
         case 'ArrowRight':
             console.log('Right arrow key pressed');
+            todd.style.left =parseInt(todd.style.left) + moveBy +'px';
             break;
     }
 });
