c0fdf2e862fc74eaf1169277a70fd1d50ce7349a
AndersRKvamme <rka092024@gmail.com>
2025-01-14 14:45:03 +0100

    Most corners fixed.

--- js.js
+++ js.js
@@ -1,5 +1,10 @@
 const todd = document.querySelector("#btnMoveMe");
 // const todd ={x:0,y:0,ele:{}}
+const pos = { x: 0, y: 0 }; // Initialize position object
+
+const move = () => {
+  todd.style.translate = `${pos.x}px ${pos.y}px`;
+};
 
 let moveBy = 10;
 
@@ -14,30 +19,57 @@ window.addEventListener('load', ()=> {
 })
 
 function test() {
-    console.log("test")
+    console.log("Moved") 
 }
 
 console.log(innerHeight)
 console.log(innerWidth)
-// InnerHeight and InnerWidth shows window size for collision. DO NOT HARDCODE.
+if (pos.y < 0) {
+  pos.y+=10
+  move()
+}
 
+// InnerHeight and InnerWidth shows window size for collision. DO NOT HARDCODE.
+// x horisontalt   y vertikalt - Less than <, greater than >
 window.addEventListener("keydown", function(event) {
     switch (event.key) {
         case 'ArrowUp':
-            console.log('Up arrow key pressed');
-            todd.style.top =parseInt(todd.style.top) - moveBy +'px';
+            //console.log('Up arrow key pressed');
+            //todd.style.top =parseInt(todd.style.top) - moveBy +'px';
+            pos.y -=10
+            console.log(pos);
+            move()
+              if (pos.y < 0) {
+                pos.y=0 
+              
+               move()
+               console.log("Stop going up.")
+             }
             break;
         case 'ArrowDown':
-            console.log('Down arrow key pressed');
-            todd.style.top =parseInt(todd.style.top) + moveBy +'px';
+            pos.y +=10
+            console.log(pos);
+            move()
             break;
         case 'ArrowLeft':
-            console.log('Left arrow key pressed');
-            todd.style.left =parseInt(todd.style.left) - moveBy +'px';
+            pos.x -=10
+            console.log(pos);
+            move()
+            if (pos.x < 0) {
+              pos.x=0 
+             move()
+             console.log("Stop going left.")
+           } else if (pos.x <0 && pos.y < 0) {
+            pos.x = 0
+            pos.y = 0
+            move()
+            console.log("Why?")
+           }
             break;
         case 'ArrowRight':
-            console.log('Right arrow key pressed');
-            todd.style.left =parseInt(todd.style.left) + moveBy +'px';
+            pos.x +=10
+            console.log(pos);
+            move()
             break;
     }
 });
@@ -45,49 +77,73 @@ window.addEventListener("keydown", function(event) {
 
 // moves todd to the clicked position - lent from Hector
 
-
-
-const pos = { x: 0, y: 0 }; // Initialize position object
-
-const move = () => {
-  todd.style.translate = `${pos.x}px ${pos.y}px`;
-};
-
+// x horisontalt   y vertikalt - Less than <, greater than >
 addEventListener("click", (toddTest) => {
   // Get click coordinates
-  pos.x = toddTest.clientX;
-  pos.y = toddTest.clientY;
+  pos.x = toddTest.clientX - todd.offsetWidth / 2;
+  pos.y = toddTest.clientY - todd.offsetHeight / 2;
+   if (pos.y < 0 && pos.x >=0 || pos.x+165 > innerWidth && pos.y < 0 ) {
+      pos.y=0 
+  
+    move()
+    console.log("Stop going up.")
+  }  else if (pos.x < 0 && pos.y >=0) {
+    pos.x=0 
 
-  // Center the element
-  pos.x -= todd.offsetWidth / 2;
-  pos.y -= todd.offsetHeight / 2;
-
+  move()
+  console.log("Stop going left.")
+}else if (pos.x < 0 && pos.y < 0) {
+    pos.x = 0
+    pos.y = 0
+    move()
+    console.log("Why?")
+   } else if (pos.x+165 > innerWidth && pos.y+165 > innerHeight) {
+    pos.x = innerWidth -165
+    pos.y = innerHeight - 165
+    move()
+    console.log("You can't escape.")
+   } else if (pos.x+165 < innerWidth && pos.y+165 > innerHeight) {
+    pos.y = innerHeight - 165
+    move()
+    console.log("No going down.")
+   } else if (pos.x+165 > innerWidth && pos.y+165 < innerHeight) {
+    pos.x = innerWidth - 165
+    move()
+    console.log("Stop going right.")
+   } else if (pos.x+165 > innerWidth && pos.y < 0) {
+    pos.x = innerWidth -165
+    pos.y = 0
+    move()
+    console.log("North East, no.")
+  }
   move();
 });
 
-
-todd.addEventListener('mousedown', mouseDownHandler);
-todd.addEventListener('mouseup', mouseUpHandler);
-todd.addEventListener('mousemove', mouseMoveHandler);
-
-let offsetX, offsetY, drag = false;
-
+let isMoving = false
+window.addEventListener('mousedown', mouseDownHandler);
+window.addEventListener('mouseup', mouseUpHandler);
+window.addEventListener('mousemove', mouseMoveHandler);
 function mouseDownHandler(e) {
-    e.preventDefault()
-  drag = true;
-  offsetX = e.clientX - todd.getBoundingClientRect().left;
-  offsetY = e.clientY - todd.getBoundingClientRect().top;
-  todd.addEventListener('mousemove', mouseMoveHandler);
+  e.preventDefault()
+isMoving = true
 }
 
 function mouseUpHandler() {
-  drag = false;
-  todd.removeEventListener('mousemove', mouseMoveHandler);
+  isMoving = false
 }
 
 function mouseMoveHandler(e) {
-  if (drag) {
-    todd.style.left = `${e.clientX - offsetX}px`;
-    todd.style.top = `${e.clientY - offsetY}px`;
+
+  if (isMoving) {
+    pos.x = e.clientX - todd.offsetWidth / 2;
+    pos.y = e.clientY - todd.offsetHeight / 2;
+    move()
   }
 }
+
+
+// move player
+// if x less than border
+// do something
+// if y less than border
+// do something
